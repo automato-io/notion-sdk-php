fe584fce2ffb8527e8f80134cd413951e79d2251
Jaroslav Holub <jaroslav@automato.io>
2025-02-10 22:18:05 +0100

    fix: callout renderer

--- src/Blocks/Renderer/Markdown/CalloutRenderer.php
+++ src/Blocks/Renderer/Markdown/CalloutRenderer.php
@@ -16,10 +16,10 @@ final class CalloutRenderer implements BlockRendererInterface
         }
 
         $text = RichTextRenderer::render(...$block->text);
-        $markdown = MarkdownRenderer::ident("> {$text}", $depth);
+        $markdown = MarkdownRenderer::ident("> {$text}\n", $depth);
 
         foreach ($block->children as $child) {
-            $markdown .= "\n>\n> " . MarkdownRenderer::renderBlock($child, $depth);
+            $markdown .= "\n>\n> " . MarkdownRenderer::renderBlock($child, $depth + 1);
         }
 
         return $markdown;
